2938883b26bcced08e63ffc5d9192e71439efea4
Busayo Famutimi <famutimi.busayo@gmail.com>
2023-02-03 08:21:47 +0100

    Validate create customer request data

--- customer-service/src/main/java/com/famzzie/customer/service/CustomerServiceImpl.java
+++ customer-service/src/main/java/com/famzzie/customer/service/CustomerServiceImpl.java
@@ -3,12 +3,12 @@ package com.famzzie.customer.service;
 import com.famzzie.customer.api.request.CreateCustomerRequest;
 import com.famzzie.customer.api.response.CreateCustomerResponse;
 import com.famzzie.customer.entity.Customer;
+import com.famzzie.customer.exception.types.InvalidRequestBodyException;
 import com.famzzie.customer.interfaces.CustomerService;
 import com.famzzie.customer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 @RequiredArgsConstructor
@@ -17,8 +17,22 @@ public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    private boolean validateCreateCustomerRequestBody(CreateCustomerRequest createCustomerRequest) {
+        int NAME_LENGTH = 50;
+        int EMAIL_LENGTH = 50;
+        int PHONE_NUMBER_LENGTH = 15;
+        return createCustomerRequest.firstName().length() <= NAME_LENGTH &&
+                createCustomerRequest.lastName().length() <= NAME_LENGTH &&
+                createCustomerRequest.email().length() <= EMAIL_LENGTH &&
+                createCustomerRequest.phoneNumber().length() <= PHONE_NUMBER_LENGTH;
+    }
+
     @Override
     public CreateCustomerResponse createCustomer(CreateCustomerRequest createCustomerRequest) {
+        if (!validateCreateCustomerRequestBody(createCustomerRequest))
+            throw new InvalidRequestBodyException("Names and email should not exceed 50 characters " +
+                    "and phone number should not exceed 15 characters. Please try again!");
+
         Customer newCustomer = Customer.builder()
                 .firstName(createCustomerRequest.firstName())
                 .lastName(createCustomerRequest.lastName())
